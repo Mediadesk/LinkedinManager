ee8bf105577fb464120b35ec34da9396762c3f10
Pranav P <ppranav164@gmail.com>
2023-08-29 17:58:34 +0530

    Config update

--- src/config/mediadesk-linkedin.php
+++ src/config/mediadesk-linkedin.php
@@ -1,6 +1,10 @@
 <?php
 
 return [
-    'api_key'    => env('MEDIADESK_LINKEDIN_API_KEY', 'XXX'),
-    'api_secret' => env('MEDIADESK_LINKEDIN_API_SECRET', 'XXX'),
+
+    'client_id'    => env('LINKEDIN_CLIENT_ID',''),
+    'secret'       => env('LINKEDIN_CLIENT_SECRET', ''),
+    'callback'     => env('LINKEDIN_CALLBACK'),
+    'host'         => env('LINKEDIN_HOST', 'https://www.linkedin.com'),
+    'api_host'     => env('LINKEDIN_API_HOST', 'https://api.linkedin.com'),
 ];
